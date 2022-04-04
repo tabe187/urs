5634163bd51b879f3de226c9d71cf736a69d2fa8
EC2 Default User <ec2-user@ip-172-31-20-227.ec2.internal>
2022-04-04 10:24:19 +0000

    devise controller generate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  scope module:  :public do
+      resources :homes, only: [:top, :about]
+      root to: 'homes#top'
+      get 'about' => "homes#about", as: "about"
+  end    
+
 end
