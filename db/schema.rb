113767387b87d800ba8d9eba64767bca0fbc315e
EC2 Default User <ec2-user@ip-172-31-20-227.ec2.internal>
2022-05-12 12:04:37 +0000

    edit column types

--- db/schema.rb
+++ db/schema.rb
@@ -88,8 +88,8 @@ ActiveRecord::Schema.define(version: 2022_05_12_073240) do
     t.string "address"
     t.decimal "rating"
     t.integer "telephone_number"
-    t.float "latitude"
-    t.float "longitude"
+    t.decimal "latitude", precision: 12, scale: 7
+    t.decimal "longitude", precision: 12, scale: 7
     t.text "map_image"
     t.text "website"
     t.string "place_id"
